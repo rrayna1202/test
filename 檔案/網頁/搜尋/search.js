a0780797e6ef4a9b4a516d5955e714814fbeddaf
rrayna1202 <rrayna1202@gmail.com>
2026-01-19 14:29:58 +0800

    Add files via upload

---
+++ 檔案/網頁/搜尋/search.js
@@ -0,0 +1,29 @@
+// search 打字
+
+// search 勾選
+const checkbox = document.querySelector('.checkboxes checkbox');
+const confirm = document.querySelector('.confirm-button');
+let 已打勾 = [];
+let 小說 = ["世界上最透明的故事"];
+let 動漫 = ["黃昏旅店"];
+let 電影電視劇 = ["即使，這份戀情今晚就會從世界上消失"];
+
+checkbox.addEventListener("change", function(
+    if (checkbox.checked) {
+        已打勾.push();
+    }
+))
+confirm.addEventListener("click", function(
+
+))
+
+// hamburger 
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = "flex";
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = "none";
+}
---
+++ 檔案/網頁/首頁/script.js
@@ -0,0 +1,13 @@
+// search
+
+
+// hamburger 
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = "flex";
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = "none";
+}
